660b7966626433fb6c0519305f22bb677fdd71dd
Milano Gadelha Carvalho <milanogc@gmail.com>
2015-04-01 20:57:51 -0300

    ...

--- app/models/account.js
+++ app/models/account.js
@@ -12,11 +12,11 @@ Account.reopenClass({
   FIXTURES: [
    {
      id: 1,
-     name: 'Root',
+     name: 'Accounts',
      createdOn: null,
      description: null,
      parentAccount: null,
-     childAccounts: [2, 3]
+     childAccounts: [2, 3, 5]
    },
    {
      id: 2,
@@ -24,7 +24,7 @@ Account.reopenClass({
      createdOn: null,
      description: null,
      parentAccount: 1,
-     childAccounts: []
+     childAccounts: [4]
    },
    {
      id: 3,
@@ -32,8 +32,48 @@ Account.reopenClass({
      createdOn: null,
      description: null,
      parentAccount: 1,
+     childAccounts: [6]
+   },
+   {
+     id: 4,
+     name: 'Pocket',
+     createdOn: null,
+     description: null,
+     parentAccount: 2,
      childAccounts: []
-   }
+   },
+   {
+     id: 5,
+     name: 'Equity',
+     createdOn: null,
+     description: null,
+     parentAccount: 1,
+     childAccounts: []
+   },
+   {
+     id: 6,
+     name: 'Credit card',
+     createdOn: null,
+     description: null,
+     parentAccount: 3,
+     childAccounts: [7]
+   },
+   {
+     id: 7,
+     name: 'Mastercard',
+     createdOn: null,
+     description: null,
+     parentAccount: 6,
+     childAccounts: [8]
+   },
+   {
+     id: 8,
+     name: 'Santander Free',
+     createdOn: null,
+     description: null,
+     parentAccount: 7,
+     childAccounts: []
+   },
   ]
 });
 
--- app/routes/accounts.js
+++ app/routes/accounts.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.find('account');
+  	this.store.find('account');
+    return this.store.find('account', 1);
   }
 });
