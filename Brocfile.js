251d7d4e9bb1d1c75c62cc570f664a55f810ea91
Milano Gadelha Carvalho <milanogc@gmail.com>
2015-06-08 21:47:26 -0300

    ember-cli updated

--- Brocfile.js
+++ Brocfile.js
@@ -20,12 +20,16 @@ var app = new EmberApp();
 // http://discuss.emberjs.com/t/why-is-it-so-difficult-to-add-bootstrap/6682#post_9 - Milano
 app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
 app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', { destDir: 'fonts' });
 
 app.import('bower_components/select2/select2.css');
 app.import('bower_components/select2-bootstrap-css/select2-bootstrap.min.css');
-app.import('bower_components/select2/select2.min.js');
 app.import('bower_components/select2/select2.png', { destDir: 'assets' });
 app.import('bower_components/select2/select2x2.png', { destDir: 'assets' });
 app.import('bower_components/select2/select2-spinner.gif', { destDir: 'assets' });
+app.import('bower_components/select2/select2.min.js');
+
+/*app.import('bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js');
+app.import('bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css');*/
 
 module.exports = app.toTree();
