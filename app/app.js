9921b45c6d4f8bf9ac8f5f008503aec3e17431bb
Milano Gadelha Carvalho <milanogc@gmail.com>
2016-03-15 18:14:27 -0300

    Better construction of accounts for tests.

--- app/app.js
+++ app/app.js
@@ -4,113 +4,123 @@ import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 import Pretender from 'pretender';
 
-/*let server = */new Pretender(function() {
-  let accounts = [
+let root = {
+  name: 'Root',
+  createdOn: null,
+  description: null,
+  children: [
     {
-      id: 1,
-      name: 'Root',
-      createdOn: null,
-      description: null,
-      parent: null,
-      children: [2, 3, 5]
-    },
-    {
-      id: 2,
       name: 'Asset',
       createdOn: null,
       description: null,
-      parent: 1,
-      children: [4, 12]
+      children: [
+        {
+          name: 'Pocket',
+          createdOn: null,
+          description: null,
+          children: []
+        },
+        {
+          name: 'Checking account',
+          createdOn: null,
+          description: null,
+          children: [{
+              name: 'Bradesco',
+              createdOn: null,
+              description: null,
+              children: []
+            }
+          ]
+        }
+      ]
     },
     {
-      id: 3,
       name: 'Liability',
       createdOn: null,
       description: null,
-      parent: 1,
-      children: [6]
+      children: [
+        {
+          name: 'Credit card',
+          createdOn: null,
+          description: null,
+          children: [
+            {
+              name: 'Mastercard',
+              createdOn: null,
+              description: null,
+              children: [
+                {
+                  name: 'Santander Free',
+                  createdOn: null,
+                  description: null,
+                  children: []
+                },
+                {
+                  name: 'Nubank',
+                  createdOn: null,
+                  description: null,
+                  children: []
+                }
+              ]
+            },
+            {
+              name: 'Visa',
+              createdOn: null,
+              description: null,
+              children: [
+                {
+                  name: 'Santander Free',
+                  createdOn: null,
+                  description: null,
+                  children: []
+                }
+              ]
+            }
+          ]
+        }
+      ]
     },
     {
-      id: 4,
-      name: 'Pocket',
-      createdOn: null,
-      description: null,
-      parent: 2,
-      children: []
-    },
-    {
-      id: 12,
-      name: 'Checking account',
-      createdOn: null,
-      description: null,
-      parent: 2,
-      children: [13]
-    },
-    {
-      id: 13,
-      name: 'Bradesco',
-      createdOn: null,
-      description: null,
-      parent: 12,
-      children: []
-    },
-    {
-      id: 5,
       name: 'Equity',
       createdOn: null,
       description: null,
-      parent: 1,
       children: []
-    },
-    {
-      id: 6,
-      name: 'Credit card',
-      createdOn: null,
-      description: null,
-      parent: 3,
-      children: [7, 10]
-    },
-    {
-      id: 7,
-      name: 'Mastercard',
-      createdOn: null,
-      description: null,
-      parent: 6,
-      children: [8, 9]
-    },
-    {
-      id: 8,
-      name: 'Santander Free',
-      createdOn: null,
-      description: null,
-      parent: 7,
+    }
+  ]
+};
+
+function createAccountsFromTree(root) {
+  let accounts = [];
+
+  function createAccount(root) {
+    let account = {
+      id: accounts.length + 1,
+      name: root.name,
       children: []
-    },
-    {
-      id: 9,
-      name: 'Nubank',
-      createdOn: null,
-      description: null,
-      parent: 7,
-      children: []
-    },
-    {
-      id: 10,
-      name: 'Visa',
-      createdOn: null,
-      description: null,
-      parent: 6,
-      children: [11]
-    },
-    {
-      id: 11,
-      name: 'Santander Free',
-      createdOn: null,
-      description: null,
-      parent: 10,
-      children: []
-    },
-  ];
+    };
+
+    accounts.push(account);
+
+    for (let child of root.children) {
+      account.children.push(createAccount(child));
+    }
+
+    return account.id;
+  }
+
+  createAccount(root);
+  return accounts;
+}
+
+/*let server = */new Pretender(function() {
+  let accounts = createAccountsFromTree(root);
+
+  function createAccount(newAccount) {
+    newAccount.id = accounts.length + 1;
+    newAccount.parent = parseInt(newAccount.parent);
+    accounts.push(newAccount);
+    accounts.find(account => account.id === newAccount.parent).children.push(newAccount.id);
+  }
 
   let entries = [];
   let transactions = [];
@@ -129,9 +139,10 @@ import Pretender from 'pretender';
   });
 
   this.post('/accounts', function(request) {
-    var data = JSON.parse(request.requestBody);
-    var account = data.account;
-    return [201, {"Content-Type": "application/json"}, JSON.stringify({"account": account})];
+    let data = JSON.parse(request.requestBody);
+    let newAccount = data.account;
+    createAccount(newAccount);
+    return [201, {"Content-Type": "application/json"}, JSON.stringify({"account": newAccount})];
   });
 
   this.get('/accounts/:id', function(request) {
@@ -142,7 +153,6 @@ import Pretender from 'pretender';
   });
 
   this.get('/accounts/:id/entries', function(request) {
-    let data = JSON.parse(request.requestBody);
     let filteredEntries = [];
 
     for (let entry of entries) {
